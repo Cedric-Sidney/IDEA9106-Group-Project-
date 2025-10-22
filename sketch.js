529e1c2151d3b3b3aae7400c07eab0a7bacd7126
Shidi Peng <spen0924@uni.sydney.edu.au>
2025-10-22 21:11:58 +1100

    version1.2

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ class Circle {
     ellipse(this.x, this.y, this.r * 2);
   }
 
+
   // 在一条线上画多个圆
   static displayLine(count, startX, startY, stepX, stepY, r) {
     for (let i = 0; i < count; i++) {
@@ -19,17 +20,59 @@ class Circle {
       let y = startY + stepY * i;
       let c = new Circle(x, y, r);
       c.display();
+      c.displayInnerDots();
     }
   }
+  // 画内部小圆
+  displayInnerDots() {
+    fill("purple");
+    stroke("orange")
+    strokeWeight(10);
+    ellipse(this.x,this.y,this.r);
+
+    noFill();
+    strokeWeight(4);
+    ellipse(this.x,this.y,this.r/1.2);
+
+    strokeWeight(4);
+    ellipse(this.x,this.y,this.r/1.5);
+
+    noStroke();
+    fill("gray");
+    ellipse(this.x,this.y,this.r/2);
+    fill("black");
+    noStroke();
+    ellipse(this.x,this.y,2*this.r/5);
+    fill("green");
+    ellipse(this.x,this.y,this.r/3.3);
+    fill("white");
+    ellipse(this.x,this.y,this.r/6);
+  }
+
 }
 
 function setup() {
+  colors = {
+  nightIndigo: color(0, 96, 137),      // dark indigo background
+  desertRed: color(207, 60, 45),       // deep earthy red
+  fireOrange: color(245, 140, 40),     // vibrant orange, warm contrast
+  sandYellow: color(238, 200, 70),     // sandy yellow for connectors
+  jungleGreen: color(76, 165, 60),     // bright leaf green
+  coralPink: color(236, 100, 150),     // coral pink accent
+  royalPurple: color(155, 80, 180),    // saturated purple
+  oceanBlue: color(70, 130, 210),      // deep sky blue
+  whiteClay: color(250, 245, 230),     // light clay white for dots
+};
+
+
   let size = min(windowWidth, windowHeight);
+
   createCanvas(size,size); // 初始画布大小
+
 }
 
 function draw() {
-  background(220);
+  background(colors.nightIndigo);
 
   let r = width / 8; // 半径随画布宽度变化
 
@@ -86,14 +129,4 @@ function windowResized() {
   resizeCanvas(size, size);
 }
 
-colors = {
-  nightIndigo: color(30, 40, 80),      // dark indigo background
-  desertRed: color(207, 60, 45),       // deep earthy red
-  fireOrange: color(245, 140, 40),     // vibrant orange, warm contrast
-  sandYellow: color(238, 200, 70),     // sandy yellow for connectors
-  jungleGreen: color(76, 165, 60),     // bright leaf green
-  coralPink: color(236, 100, 150),     // coral pink accent
-  royalPurple: color(155, 80, 180),    // saturated purple
-  oceanBlue: color(70, 130, 210),      // deep sky blue
-  whiteClay: color(250, 245, 230),     // light clay white for dots
-};
+
